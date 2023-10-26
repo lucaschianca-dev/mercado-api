8c01836d7753b455e191ef295a26e6c8a527b78c
lucaschianca-dev <lucaseduardochianca.dev@gmail.com>
2023-10-26 13:18:16 -0300

    Projeto INDRA iniciado

---
+++ src/main/java/com/mercado/apimercado/ApiMercadoApplication.java
@@ -0,0 +1,13 @@
+package com.mercado.apimercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMercadoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiMercadoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mercado/apimercado/ApiMercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercado.apimercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMercadoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
