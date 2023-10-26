6ea841638f20b2819fcf75b55b4ef97d5d6fecbc
lucaschianca-dev <lucaseduardochianca.dev@gmail.com>
2023-10-26 13:23:55 -0300

    feat: criado enum 'Tipo'

---
+++ src/main/java/com/mercado/apimercado/enums/Tipo.java
@@ -0,0 +1,18 @@
+package com.mercado.apimercado.enums;
+
+public enum Tipo {
+    FRUTA,
+
+    FRUTO_DO_MAR,
+
+    CARNE,
+
+    HIGIENE,
+
+    LIMPEZA,
+
+    BEBIDA_ALCOOLICA,
+
+    BEBIDA_NAO_ALCOOLICA
+
+}
